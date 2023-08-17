1c0f2fc6cd0d4c12158b1d122c96240ecd63f312
Alejandro Usma <causmar97@gmail.com>
2023-08-17 00:44:00 -0500

    Added cool animations using jQuery
    * White line under the active tab in the main nav changed from border to shadow, to avoid pusing content towards the top in the active tab.

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('main, .logo').hide().fadeIn(1000);
+});
+$(document).ready(function() {
+    $('.navigation-menu, .aside-social-navigation').toggleClass('open');
+});
