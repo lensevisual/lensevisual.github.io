720046666d6cdd6209f4d2d6378e0be0bcf28eef
Alejandro Usma <causmar97@gmail.com>
2023-12-15 14:51:46 -0500

    Toggle functionality for the gallery to switch between videos and photos added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,3 +4,38 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.navigation-menu, .aside-social-navigation').toggleClass('open');
 });
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtén los elementos relevantes
+    const toggleVideo = document.getElementById('toggle-video');
+    const togglePhoto = document.getElementById('toggle-photo');
+    const videoGallery = document.getElementById('video-gallery');
+    const photoGallery = document.getElementById('photo-gallery');
+  
+    // Función para mostrar la galería de videos y ocultar la de fotos
+    function showVideoGallery() {
+      videoGallery.style.display = 'flex';
+      photoGallery.style.display = 'none';
+    }
+  
+    // Función para mostrar la galería de fotos y ocultar la de videos
+    function showPhotoGallery() {
+      videoGallery.style.display = 'none';
+      photoGallery.style.display = 'flex';
+    }
+  
+    // Función para manejar el cambio de galería cuando se hace clic en el toggle
+    function handleGalleryToggle() {
+      if (toggleVideo.checked) {
+        showVideoGallery();
+      } else {
+        showPhotoGallery();
+      }
+    }
+  
+    // Asigna el manejador de eventos al cambio de los toggles
+    toggleVideo.addEventListener('change', handleGalleryToggle);
+    togglePhoto.addEventListener('change', handleGalleryToggle);
+  
+    // Asegúrate de mostrar la galería correcta al cargar la página
+    handleGalleryToggle();
+  });
